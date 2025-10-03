34ebc69fc20ee0091d17186d616dc5bc6eb234d5
Markus Harnusek <m.harnusek@students.htl-leonding.ac.at>
2025-10-03 11:37:06 +0200

    Init

---
+++ README.md
@@ -0,0 +1,12 @@
+# Tic Tac Toe
+
+This is a simple tic tac toe game for c.
+
+## How to run it
+
+If you are on linux compile it using the following command `gcc TicTacToe.c -std=c17 -o TicTacToe` and then run it using `./TicTacToe`
+
+## Requirements
+
+ - **`gcc`**: The gcc compiler is necessary for compiling the project for the system specific hardware. For apt install it by first updating the package list with `sudo apt update` and then install it by using `sudo apt install gcc`
+ - **File being executable**: In order to run the project the file needs to have the executable permission for the current user / group. Check this by checking the permission bits when using `ls -al` and change it if necessary using `chmod -x <file-name>`
---
+++ TicTacToe.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+    
+int checkWinner(int field[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        if (field[i][0] != 0 && field[i][0] == field[i][1] && field[i][1] == field[i][2]) {
+            return field[i][0];
+        } 
+
+        if (field[0][i] != 0 && field[0][i] == field[1][i] && field[1][i] == field[2][i]) {
+            return field[0][i];
+        } 
+    }
+
+    if (field[0][0] != 0 && field[0][0] == field[1][1] && field[1][1] == field[2][2]) {
+        return field[0][0];
+    }
+
+    if (field[0][2] != 0 && field[0][2] == field[1][1] && field[1][1] == field[2][0]) {
+        return field[0][2];
+    }
+
+    return 0;
+}
+
+const char* printField(int field[3][3]) {
+    static char result[100] = "";
+    strcpy(result, "+---+---+---+\n");
+
+    for (int i = 0; i < 3; i++) {
+        strcat(result, "|");
+
+        for (int j = 0; j < 3; j++) {
+            switch (field[i][j]) {
+                case 0:
+                    strcat(result, "   |");
+                    break;
+
+                case 1:
+                    strcat(result, " X |");
+                    break;
+
+                case 2:
+                    strcat(result, " O |");
+                    break;
+            }
+        }  
+        
+        strcat(result, "\n+---+---+---+\n");
+    }
+
+    return result;
+}
+
+int main(int argc, char* argv[]) {
+    int field[3][3] ={
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    };
+
+    int currentPlayer = 2;
+
+    while (checkWinner(field) == 0) {
+        printf("\033[2J\033[H");
+        currentPlayer = currentPlayer == 1 ? 2 : 1;
+        printf("%s\n", printField(field));
+        
+        printf("\n--- Player %d ---\n", currentPlayer);
+
+        bool inputValid = false;
+        int x = 0;
+        int y = 0;
+
+        while (!inputValid)
+        {
+            printf("\nEnter x position: ");
+            scanf("%d", &x);
+            printf("\nEnter y position: ");
+            scanf("%d", &y);
+
+            if (field[x][y] == 0) {
+                inputValid = true;
+                field[x][y] = currentPlayer;
+            } else {
+                printf("\nPosition invalid. Try again.\n");
+            }
+        }
+
+    }
+
+    printf("\nPlayer %d won!\n", currentPlayer);
+}
